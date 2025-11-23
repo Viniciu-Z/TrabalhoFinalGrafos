d451d19db194dc7a6c5813e004ca02854e7d4780
Viniciu-Z <prudenciovinicius587@gmail.com>
2025-11-23 16:30:36 -0300

    criados função que retorna todos os vizinho de um vertice e o grau do vertice

--- Dicionario.py
+++ Dicionario.py
@@ -1,3 +1,4 @@
+import Grafos
 from Grafos import *
 
 def verticesNoGrafo(grafo):
@@ -14,4 +15,25 @@ def arestasNoGrafo(grafo):
             arestas.add(aresta)
     return len(arestas)
 
+def vizinhanca(grafo, vertice):
+    vizinhos = []
+    for destino, peso in grafo.vertices[vertice]: #destino são os vertices InNeighbor
+        if destino not in vizinhos:
+            vizinhos.append(destino)
 
+    for chegando in grafo.vertices: #chegando são os vertices OutNeighbor
+        for destino, peso in grafo.vertices[chegando]:
+            if destino == vertice and chegando not in vizinhos:
+                vizinhos.append(chegando)
+    return vizinhos
+
+def graus(grafo, vertice):
+    grau = 0
+    for chegando in grafo.vertices:
+        if chegando == vertice:
+            grau += len(grafo.vertices[chegando])
+
+        for vizinho, peso in grafo.vertices[chegando]:
+            if vizinho == vertice:
+                grau += 1
+    return grau
--- Main.py
+++ Main.py
@@ -1,16 +1,33 @@
 from Grafos import *
 from Dicionario import *
 
-grafo = Grafo(ponderado=True)
+def main():
+    print("=== Grafo Não Direcionado ===")
+    grafo = Grafo(ponderado=True)
 
-grafo.adicionarConexoes("A", "B", 3)
-grafo.adicionarConexoes("A", "C", 2)
-grafo.adicionarConexoes("B", "D", 5)
-grafo.adicionarConexoes("C", "D", 1)
-grafo.adicionarConexoes("D", "E", 2)
-grafo.adicionarConexoes("E", "A", 3)
+    grafo.adicionarConexoes("A", "B", 3)
+    grafo.adicionarConexoes("A", "C", 2)
+    grafo.adicionarConexoes("B", "D", 5)
+    grafo.adicionarConexoes("C", "D", 1)
 
-grafo.mostrarGrafo()
+    grafo.mostrarGrafo()
 
-print(f"quantidade de vertices no grafo {verticesNoGrafo(grafo)}")
-print(f"quantidade de arestas no grafo {arestasNoGrafo(grafo)}")
+    print("\n=== Digrafo (Direcionado) ===")
+    digrafo = Digrafo.Grafo(ponderado=True)
+
+    digrafo.adicionarConexoes("A", "B", 4)  # A -> B
+    digrafo.adicionarConexoes("A", "C", 1)  # A -> C
+    digrafo.adicionarConexoes("B", "D", 7)  # B -> D
+    digrafo.adicionarConexoes("C", "D", 3)  # C -> D
+    digrafo.adicionarConexoes("D", "E", 1)  # D -> E
+
+    digrafo.mostrarGrafo()
+
+    # print(f"quantidade de vertices no grafo {verticesNoGrafo(grafo)}")
+    # print(f"quantidade de arestas no grafo {arestasNoGrafo(grafo)}")
+    # print(vizinhanca(digrafo, "E"))
+    print(graus(digrafo, "E"))
+
+
+if __name__ == "__main__":
+    main()
